e550f631e571b0a62925de9e000ff19f22c93ef6
Jubu43 <emmanuelolajubu43@gmail.com>
2022-09-12 14:39:30 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * *
+ *
+ * * Description: using the main function
+ *
+ * * this program prints "Programming is like building a multilingual puzzle
+ *
+ * * Return: 0
+ *
+ * 
+ */
+
+int main(void)
+{
+	printf('with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
